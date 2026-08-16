855fea5384cffd023c8b25b21984d2c45844234b
iamendless777 <iamendless777@gmail.com>
2026-08-16 12:54:10 -0500

    feat(motion): export runtime cues and player

--- src/motion/index.ts
+++ src/motion/index.ts
@@ -3,3 +3,5 @@ export * from "./tumble.js";
 export * from "./styles.js";
 export * from "./assess.js";
 export * from "./timeline.js";
+export * from "./runtime-cues.js";
+export * from "./player.js";
