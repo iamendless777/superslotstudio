aa2823bcf7ec146640712c71f13d031082a51105
Christian Parker <phantommac@Christians-iMac.local>
2026-08-17 12:33:11 -0500

    feat(preview): use PreviewPanelMotion for Play Motion controls

--- runtime/stake-studio-source/src/app.js
+++ runtime/stake-studio-source/src/app.js
@@ -3,7 +3,7 @@ import { generateDefaultPaylines, getExecutableWinType } from './engines/math/Wi
 import { GAME_TYPES, BONUS_MECHANICS, getCompatibleMechanics } from './mechanics/registry.js';
 import { CabinetEditor } from './editor/cabinet/CabinetEditor.js';
 import { ConfigPanel } from './editor/configurator/ConfigPanel.js';
-import { PreviewPanel } from './editor/preview/PreviewPanel.js?orchestration=20260815-40';
+import { PreviewPanel } from './editor/preview/PreviewPanelMotion.js';
 import { ReelStripEditor } from './editor/reelstrip/ReelStripEditor.js';
 import { SimulationDashboard } from './editor/simulation/SimulationDashboard.js';
 import { PaytableValidator } from './engines/math/PaytableValidator.js';
